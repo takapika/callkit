96279cad1610affb86d94a229dead3ae4bfcf7de
takapika <taka2488@gmail.com>
2018-05-04 08:56:14 +0900

    project commit

---
+++ CallKitSample/CallKitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallKitSample
+//
+//  Created by lcs-developer on 2018/05/04.
+//  Copyright © 2018年 takapika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
